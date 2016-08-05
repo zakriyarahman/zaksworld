2fcff3761ce634382f40edc082e5e636a3255ed6
Zak <zakriyarahman@gmail.com>
2016-08-05 15:20:09 -0500

    Changing public directory access from public/ to public_html/
    Changing public directory access from public/ to public_html/. This is
    done to comply with the servers public access. Subsequently, the config
    file of the application is also changed to reflect changes.

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('app/public_html'),
             'visibility' => 'public',
         ],
 
